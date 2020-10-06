05c1cf6dccea8f6ae77042e1c28627f696b9e3c1
Joao Vitor de S.S <Joao.Simao@student.nhh.no>
2020-10-06 21:58:48 +0200

    Created R_template.R

---
+++ R_template.R
@@ -0,0 +1,14 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+#install libraries ----
+
+library(dplyr)
+
+#load data frame ----
+DF <- read.csv("") %>% 
+  rename() %>% 
+  mutate() 
+
+
+# Anything else ----
